a8f285810ce95cb62c75f907bc17893ebaa7135c
Derek Ethier <derek.ethier@gmail.com>
2013-08-08 14:07:16 -0400

    Disable override

--- app/overrides/admin/admin-order-show-buttons.rb
+++ app/overrides/admin/admin-order-show-buttons.rb
@@ -3,4 +3,4 @@ Deface::Override.new(:virtual_path => 'spree/orders/show',
 :name => 'admin-order-show-buttons',
 :insert_after => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
 :partial => "spree/hooks/wholesale_customer_id",
-:disabled => false)
+:disabled => true)
